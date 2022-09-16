ae9fc4052a6681cb5fd9522c0b0f9d5d3db5b938
maniac <maniacflow.dev@gmail.com>
2022-09-16 14:28:35 +0800

    添加 Container 组件

---
+++ docs/.vuepress/components/Container.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        props: ['type', 'title']
+    }
+</script>
+
+<template>
+    <div v-if="type==='details'">
+        <details class="custom-details">
+            <summary>{{title}}</summary>
+            <p>
+                <slot></slot>
+            </p>
+        </details>
+    </div>
+    <div v-else>
+        <div :class="'custom-block ' + type">
+            <p class="custom-block-title">{{title}}</p>
+            <p>
+                <slot></slot>
+            </p>
+        </div>
+    </div>
+</template>
