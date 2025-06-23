86d6a4a4a2058faaccacc6bc7259f3679171887c
dujiepeng <347302029@qq.com>
2025-06-23 12:49:14 +0800

    修改请求地址为沙箱

--- docs/.vuepress/components/Feedback.vue
+++ docs/.vuepress/components/Feedback.vue
@@ -210,7 +210,7 @@ const submitFeedback = async () => {
 
     let response, result
     try {
-      response = await fetch('http://localhost:3000/api/feedback', {
+      response = await fetch('http://119.28.130.183:3000/api/feedback', {
         method: 'POST',
         body: formData,
         headers: {
