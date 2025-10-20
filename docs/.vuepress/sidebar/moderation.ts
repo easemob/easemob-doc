1a21ca0b5e5ef6638891985e893ea2b0c03860c3
haoxiuwen <amy.hao@easemob.com>
2025-10-20 15:29:33 +0800

    modify

--- docs/.vuepress/sidebar/moderation.ts
+++ docs/.vuepress/sidebar/moderation.ts
@@ -4,7 +4,7 @@ export const MODERATION_SIDEBAR = [
           text: "产品定价",
           collapsible: true,
           children: [
-            {
+            { 
               text: "国内计费说明",
               link: "/value-added/moderation/moderation_billing_domestic.html",
             },
@@ -15,20 +15,14 @@ export const MODERATION_SIDEBAR = [
           ],
         },
         { text: "开通审核服务", link: "/value-added/moderation/moderation_enable.html" },
-        {
-              text: "规则配置",
-              link: "/value-added/moderation/moderation_rule_config.html",
-            },
+        { text: "规则配置", link: "/value-added/moderation/moderation_rule_config.html" },
         { text: "规则测试", link: "/value-added/moderation/moderation_rule_test.html" },
         { text: "数据总览", link: "/value-added/moderation/moderation_data_overview.html" },
         { text: "历史记录", link: "/value-added/moderation/moderation_history.html" },
         { text: "消息举报", link: "/value-added/moderation/moderation_message_report.html" },
         { text: "关键词审核", link: "/value-added/moderation/moderation_keyword.html" },
         { text: "标签管理", link: "/value-added/moderation_user_tag.html" },
-        {
-              text: "消息审核机制",
-              link: "/value-added/moderation/moderation_mechanism.html",
-        },
+        { text: "消息审核机制", link: "/value-added/moderation/moderation_mechanism.html"},
         {
           text: "进阶功能",
           collapsible: true,
