2c564acb9fdefaaaced2a3ecf0a77f64d52021b9
haoxiuwen <amy.hao@easemob.com>
2025-10-20 11:21:20 +0800

    modify

--- docs/.vuepress/navbar/index.ts
+++ docs/.vuepress/navbar/index.ts
@@ -253,10 +253,10 @@ export const zhNavbar = navbar([
   {
     text: '增值扩展',
       children: [
-        { text: '内容审核', link: '/document/server-side/overview.html'},
+        { text: '内容审核', link: '/value-added/moderation/moderation_overview.html'},
         { text: '实时音视频', link: '/callkit/android/product_activation.html'},
-        { text: '消息翻译', link: '/product/message_translation_android.html'},
-        { text: '即时推送', link: 'value-added/push/push_overview.html'}
+        { text: '消息翻译', link: '/value-added/translation/message_translation_android.html'},
+        { text: '即时推送', link: '/value-added/push/push_overview.html'}
       ]
   },
   // {
