47f7683f1570522713f9b59c3ab74ecae1597bb8
wangyuhui <wangyuhui727@163.com>
2025-12-01 21:59:52 +0800

    ts文件格式还原，首页图片压缩，内容页样式按需调整，右侧滚动栏取消单行限定

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,13 +1,13 @@
-import { defineUserConfig, UserConfig } from "vuepress";
-import { viteBundler } from "@vuepress/bundler-vite";
-import { hopeTheme } from "vuepress-theme-hope";
+import { defineUserConfig, UserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { hopeTheme } from 'vuepress-theme-hope'
 // import AutoImport from 'unplugin-auto-import/vite'
 // import Components from 'unplugin-vue-components/vite'
 // import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { docsearchPlugin } from "@vuepress/plugin-docsearch";
-import { containerPlugin } from "./markdown/plugin";
-import theme from "./theme.js";
-import path from "node:path";
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+import { containerPlugin } from './markdown/plugin'
+import theme from './theme.js'
+import path from 'node:path'
 
 // import type { DocSearchProps } from '@docsearch/react'
 
@@ -23,18 +23,18 @@ import path from "node:path";
 // const defineDocSearchConfig: (options: DocSearchClientOptions) => void
 
 export default defineUserConfig({
-  base: "/",
-  lang: "zh-CN",
-  title: " IM 文档",
-  head: [["link", { rel: "icon", href: "/logo.png" }]],
-  description: "环信 IM 文档",
+  base: '/',
+  lang: 'zh-CN',
+  title: ' IM 文档',
+  head: [['link', { rel: 'icon', href: '/logo.png' }]],
+  description: '环信 IM 文档',
   shouldPrefetch: false,
   bundler: viteBundler({
     viteOptions: {
       resolve: {
         alias: {
-          "@static": path.resolve(__dirname, "./public"),
-        },
+          '@static': path.resolve(__dirname, './public')
+        }
       },
       plugins: [
         // AutoImport({
@@ -43,126 +43,126 @@ export default defineUserConfig({
         // Components({
         //   resolvers: [ElementPlusResolver({ssr: true })],
         // }),
-      ],
+      ]
     },
-    vuePluginOptions: {},
+    vuePluginOptions: {}
   }),
   markdown: {
     headers: {
-      level: [2, 3],
+      level: [2, 3]
     },
     code:{
       lineNumbers:false
     }
   },
   extendsMarkdown: (md) => {
-    containerPlugin(md);
+    containerPlugin(md)
   },
-  templateBuild: path.resolve(__dirname, "./templates/index.build.html"),
+  templateBuild: path.resolve(__dirname, './templates/index.build.html'),
   theme,
   plugins: [
     docsearchPlugin({
-      appId: "5K8UTB3JVE",
-      apiKey: "df9e938d06f6531ce8dd8de71f907f0d",
-      indexName: "im-beta-easemob",
+      appId: '5K8UTB3JVE',
+      apiKey: 'df9e938d06f6531ce8dd8de71f907f0d',
+      indexName: 'im-beta-easemob',
       searchParameters: {
         attributesToSnippet: [
-          "hierarchy.lvl1:20",
-          "hierarchy.lvl2:20",
-          "hierarchy.lvl3:20",
-          "hierarchy.lvl4:20",
-          "hierarchy.lvl5:20",
-          "hierarchy.lvl6:20",
-          "content:50",
+          'hierarchy.lvl1:20',
+          'hierarchy.lvl2:20',
+          'hierarchy.lvl3:20',
+          'hierarchy.lvl4:20',
+          'hierarchy.lvl5:20',
+          'hierarchy.lvl6:20',
+          'content:50'
         ],
         hitsPerPage: 50,
         facetFilters: [
           [
-            "type:lvl0",
-            "type:lvl1",
-            "type:lvl2",
-            "type:lvl3",
-            "type:lvl4",
-            "type:lvl5",
-          ],
-        ],
+            'type:lvl0',
+            'type:lvl1',
+            'type:lvl2',
+            'type:lvl3',
+            'type:lvl4',
+            'type:lvl5'
+          ]
+        ]
       },
       maxResultsPerGroup: 30,
       // transformItems: (items) => {
       //   console.log(items)
       //   return items.map((items) => ({ ...items, content: 'xxxxxx' }))
       // },
-      placeholder: "搜索文档",
+      placeholder: '搜索文档',
       translations: {
         button: {
-          buttonText: "搜索",
-          buttonAriaLabel: "搜索文档",
+          buttonText: '搜索',
+          buttonAriaLabel: '搜索文档'
         },
         modal: {
           searchBox: {
-            resetButtonTitle: "清除查询条件",
-            resetButtonAriaLabel: "清除查询条件",
-            cancelButtonText: "取消",
-            cancelButtonAriaLabel: "取消",
+            resetButtonTitle: '清除查询条件',
+            resetButtonAriaLabel: '清除查询条件',
+            cancelButtonText: '取消',
+            cancelButtonAriaLabel: '取消'
           },
           startScreen: {
-            recentSearchesTitle: "搜索历史",
-            noRecentSearchesText: "没有搜索历史",
-            saveRecentSearchButtonTitle: "保存至搜索历史",
-            removeRecentSearchButtonTitle: "从搜索历史中移除",
-            favoriteSearchesTitle: "收藏",
-            removeFavoriteSearchButtonTitle: "从收藏中移除",
+            recentSearchesTitle: '搜索历史',
+            noRecentSearchesText: '没有搜索历史',
+            saveRecentSearchButtonTitle: '保存至搜索历史',
+            removeRecentSearchButtonTitle: '从搜索历史中移除',
+            favoriteSearchesTitle: '收藏',
+            removeFavoriteSearchButtonTitle: '从收藏中移除'
           },
           errorScreen: {
-            titleText: "无法获取结果",
-            helpText: "你可能需要检查你的网络连接",
+            titleText: '无法获取结果',
+            helpText: '你可能需要检查你的网络连接'
           },
           footer: {
-            selectText: "选择",
-            navigateText: "切换",
-            closeText: "关闭",
-            searchByText: "搜索提供者",
+            selectText: '选择',
+            navigateText: '切换',
+            closeText: '关闭',
+            searchByText: '搜索提供者'
           },
           noResultsScreen: {
-            noResultsText: "无法找到相关结果",
-            suggestedQueryText: "你可以尝试查询",
-            reportMissingResultsText: "你认为该查询应该有结果？",
-            reportMissingResultsLinkText: "点击反馈",
-          },
-        },
-      },
-    }),
+            noResultsText: '无法找到相关结果',
+            suggestedQueryText: '你可以尝试查询',
+            reportMissingResultsText: '你认为该查询应该有结果？',
+            reportMissingResultsLinkText: '点击反馈'
+          }
+        }
+      }
+    })
   ],
   onPrepared: async (app) => {
     await app.writeTemp(
-      "pages.js",
+      'pages.js',
       `export default ${JSON.stringify(app.pages.map(({ data }) => data))}`
-    );
+    )
   },
   alias: {
-    "@theme-hope/components/HomePage": path.resolve(
+    '@theme-hope/components/HomePage': path.resolve(
       __dirname,
-      "./components/HomePage.vue"
+      './components/HomePage.vue'
     ),
 
-    "@theme-hope/modules/sidebar/components/Sidebar": path.resolve(
+    '@theme-hope/modules/sidebar/components/Sidebar': path.resolve(
       __dirname,
-      "./components/Sidebar.vue"
+      './components/Sidebar.vue'
     ),
 
-    "@theme-hope/modules/navbar/components/Navbar": path.resolve(
+    '@theme-hope/modules/navbar/components/Navbar': path.resolve(
       __dirname,
-      "./components/Navbar.vue"
+      './components/Navbar.vue'
     ),
 
-    "@theme-hope/components/MarkdownContent": path.resolve(
+    '@theme-hope/components/MarkdownContent': path.resolve(
       __dirname,
-      "./components/MarkdownContent.vue"
+      './components/MarkdownContent.vue'
     ),
 
-    "@theme-hope/components/PageNav": path.resolve(
+    '@theme-hope/components/PageNav': path.resolve(
       __dirname,
-      "./components/PageNav.vue"
-    ),
-  },
-});
+      './components/PageNav.vue'
+    )
+  }
+})
--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -1,58 +1,57 @@
-import { hopeTheme, ThemeOptions } from "vuepress-theme-hope";
-import { zhNavbar } from "./navbar/index.js";
-import { zhSidebar } from "./sidebar/index.js";
+import { hopeTheme, ThemeOptions } from 'vuepress-theme-hope'
+import { zhNavbar } from './navbar/index.js'
+import { zhSidebar } from './sidebar/index.js'
 
 interface CustomConfig {
-  extra_nav?: any[];
+  extra_nav?: any[]
 }
 
 export default hopeTheme(<ThemeOptions & CustomConfig>{
-  hostname: "https://doc.easemob.com/",
-  home: "/",
-  iconAssets: "iconfont",
-  logo: "/logo_name.png",
-  repo: "easemob/easemob-doc",
-  docsBranch: "doc-v2",
-  docsDir: "docs",
-  darkmode: "disable",
+  hostname: 'https://doc.easemob.com/',
+  home: '/',
+  iconAssets: 'iconfont',
+  logo: '/logo_name.png',
+  repo: 'easemob/easemob-doc',
+  docsBranch: 'doc-v2',
+  docsDir: 'docs',
+  darkmode: 'disable',
   pure: true,
   contributors: false,
   // navbar
   navbar: zhNavbar,
   navbarLayout: {
-    start: ["Brand", "Links"],
+    start: ['Brand','Links'],
     center: [],
-    end: ["Language", "Repo", "Outlook"],
+    end: ['Language', 'Repo', 'Outlook']
   },
   // sidebar
   sidebar: zhSidebar,
-  footer:
-    "<div class='footer-left'>环信 IM 文档 V 1.0.0</div><div class='footer-right'>©️环信 2026</div>",
+  footer: '环信 IM 文档 Version: 1.0.0 ©️环信',
   displayFooter: true,
   headerDepth: 2,
   extra_nav: [
     // { text: '提交工单', link: 'https://console.easemob.com/ticket', type: 'info' },
     {
-      text: "登录",
-      link: "https://console.easemob.com/user/login",
-      type: "",
+      text: '登录',
+      link: 'https://console.easemob.com/user/login',
+      type: 'success'
     },
     {
-      text: "注册",
-      link: "https://console.easemob.com/user/register",
-      type: "primary",
-    },
+      text: '注册',
+      link: 'https://console.easemob.com/user/register',
+      type: 'primary'
+    }
   ],
-  editLink: false,
+  editLink:false,
   plugins: {
     mdEnhance: {
       container: true,
       imgSize: true,
-      tabs: true,
+      tabs: true
     },
     prismjs: {
       light: "vsc-dark-plus", // 使用 VS Code Dark+ 主题(暗色背景,适合技术文档)
       dark: "vsc-dark-plus", // 暗色模式下使用相同主题
     },
-  },
-});
+  }
+})
