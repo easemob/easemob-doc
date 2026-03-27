0276a58f32dd9233732b4cfb7569026f7c2126f1
haoxiuwen <amy.hao@easemob.com>
2026-03-27 13:47:21 +0800

    modify

--- docs/.vuepress/sidebar/uikit.ts
+++ docs/.vuepress/sidebar/uikit.ts
@@ -289,8 +289,8 @@ const chatUikitSidebar = [
   },
 ];
 const chatroomUikitSidebar = [
-  { text: "产品概述", link: "roomuikit_overview.html" },
   { text: "使用 MCP 集成", link: "easemob_mcp_server.html"},
+  { text: "产品概述", link: "roomuikit_overview.html" },
   {
     text: "特性",
     collapsible: true,
@@ -309,8 +309,7 @@ const chatroomUikitSidebar = [
   { text: "主题", link: "roomuikit_theme.html" },
   { text: "自定义", link: "roomuikit_customize.html" },
   { text: "组件文档", link: "roomuikit_storybook.html", only: ["web"] },
-  { type: "separator", except: ["web"] } as any,
-  { type: "separator", only: ["web"]} as any,
+  { type: "separator" } as any,
   { text: "更新日志", link: "roomuikit_releasenote.html" },
   { text: "常见问题", link: "faq.html", only: ["react-native"] },
   { text: "设计指南", link: "design_guide.html" },
