4f29f33b2d95412a4efc948ba0cd55f21bc13275
haoxiuwen <amy.hao@easemob.com>
2026-09-18 11:56:19 +0800

    Add Banner to Highlight SDK V5

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -231,6 +231,11 @@ export default defineUserConfig({
       './components/MarkdownContent.vue'
     ),
 
+    '@theme-hope/components/PageTitle': path.resolve(
+      __dirname,
+      './components/PageTitle.vue'
+    ),
+
     '@theme-hope/components/PageNav': path.resolve(
       __dirname,
       './components/PageNav.vue'
